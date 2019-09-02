7eff2b8c21cb76d4f069158e2a37d84058584359
Nikema Prophet <nikema.prophet@popschools.com>
2019-09-02 02:07:32 -0700

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+const functions = require("firebase-functions")
+const app = require("express")()
+const FBAuth = require("./util/fbAuth")
+
+const { getAllScreams, postOneScream } = require("./handlers/screams")
+const { signup, login } = require("./handlers/users")
+
+// Scream routes
+app.get("/screams", getAllScreams)
+app.post("/scream", FBAuth, postOneScream)
+
+// Users routes
+app.post("/signup", signup)
+app.post("/login", login)
+
+exports.api = functions.https.onRequest(app)
---
+++ functions/util/config.sample.js
@@ -0,0 +1,10 @@
+// Paste your firebase config here and rename to config.js
+// module.exports = {
+//   apiKey: "",
+//   authDomain: "",
+//   databaseURL: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: ""
+// }
